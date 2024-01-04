ab0eb187b3db7f5d3dd1183de96fbee0e182d4a5
Andrius da Silva <andrius.dasilva@thoughtworks.com>
2024-01-04 16:55:56 -0300

    create project

---
+++ shared/src/commonTest/kotlin/com/andriuswill/spacemissionkmp/Test.kt
@@ -0,0 +1,12 @@
+package com.andriuswill.spacemissionkmp
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class CommonGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greet().contains("Hello"), "Check 'Hello' is mentioned")
+    }
+}
